8e3f6b535a95bdc7996c3e9580adcddb142ee71b
linesh <linesh.simpcity@gmail.com>
2016-09-05 20:37:29 +0800

    Linesh: Fetch fake data from local

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Questions from './data.json'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+   render() {
+      console.info(this.props)
+      return (
+         <div className="App">
+            <div className="question-panel">
+               welcome {this.props.data}
+            </div>
+         </div>
+      )
+   }
 }
+App.defaultProps = {
+   data: Questions.Hello
+}
+
 
 export default App;
