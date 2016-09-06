ee770484b8fac97c7665e3100e3d27353fe2d9e9
linesh <linesh.simpcity@gmail.com>
2016-09-06 20:41:19 +0800

    [#4] Use label-radio to gather user selected answer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Question from './pages/Question'
+import Questions from './data.json'
 import './App.css';
 
 class App extends Component {
+   componentWillMount() {
+      this.props = {
+         data: Questions.map((question, index) => {
+            question.index = index
+            question.answer = ''
+            return question
+         }),
+         current: {
+            index: 0,
+            question: Questions[0]
+         }
+      }
+   }
 
    render() {
       return (
-         <Question></Question>
+         <div className="App">
+            <Question question={this.props.current.question} onNext={this.handleNext.bind(this)}></Question>
+         </div>
       )
+   }
+
+   handleNext(question) {
+      console.log(question)
+   }
+
+   handlePrevious(question) {
+
    }
 }
 
--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react'
-import Questions from '../data.json'
 
 class Question extends Component {
-   componentWillMount() {
-      this.props = {
-         data: Questions
-      }
+
+   onNext() {
+      this.props.onNext({
+         id: this.props.question.id,
+         index: this.props.question.index,
+         answer: this.props.question.answer
+      })
+   }
+
+   onSelect(event) {
+      this.props.question.answer = event.target.value
    }
 
    render() {
-      console.info(this.props)
       return (
-         <div className="App">
-            <div className="question-panel">
-               <div className="question">{ this.props.data[0].question }</div>
-               <div className="question-image">{ this.props.data[0].images }</div>
-               <div className="options">
-                  {
-                     this.props.data[0].options.map((option, index) => {
-                        return <div className="option">{option}</div>
-                     })
-                  }
-               </div>
-               <div className="question-navigation">
-                  <button type="button" className="previous-question-button">上一题</button>
-                  <button type="button" className="next-question-button">下一题</button>
-               </div>
+         <div className="question-panel">
+            <div className="question">{ this.props.question.question }</div>
+            <div className="question-image">{ this.props.question.images }</div>
+            <div className="options">
+               {
+                  this.props.question.options.map((option, index) => {
+                     return (
+                        <div className="option" key={index}>
+                           <label htmlFor={ `option${index}` }>{option}</label>
+                           <input id={ `option${index}` } type="radio" value={option} name="option" onChange={this.onSelect.bind(this)}/>
+                        </div>
+                     )
+                  })
+               }
+            </div>
+            <div className="question-navigation">
+               <button type="button" className="previous-question-button">上一题</button>
+               <button type="button" className="next-question-button" onClick={this.onNext.bind(this)}>下一题</button>
             </div>
          </div>
       )
